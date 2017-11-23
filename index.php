c2e8626fbb6d7461c80f62dd3d7e523aae821e3d
Pedro Dib <pedrohenrique635@hotmail.com>
2017-11-23 18:46:52 -0200

    My website

---
+++ index.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->  
+<!--[if IE 9]> <html lang="en" class="ie9"> <![endif]-->  
+<!--[if !IE]><!--> <html lang="en"> <!--<![endif]-->  
+<head>
+    <title>Pedro Dib - Full Stack Web Developer</title>
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Responsive HTML5 Resume/CV Template for Developers">
+    <meta name="author" content="Pedro Dib">    
+    <link rel="shortcut icon" href="favicon.ico">  
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,500,400italic,300italic,300,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+    <!-- Global CSS -->
+    <link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">   
+    <!-- Plugins CSS -->
+    <link rel="stylesheet" href="assets/plugins/font-awesome/css/font-awesome.css">
+    
+    <!-- Theme CSS -->  
+    <link id="theme-style" rel="stylesheet" href="assets/css/styles-3.css">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head> 
+
+<body>
+    <div class="wrapper">
+        <div class="sidebar-wrapper">
+            <div class="profile-container">
+                <img class="profile img-circle" src="assets/images/me.jpg" alt="" />
+                <h1 class="name">Pedro Dib</h1>
+                <h3 class="tagline">Full Stack Web Developer</h3>
+            </div><!--//profile-container-->
+            
+            <div class="contact-container container-block">
+                <ul class="list-unstyled contact-list">
+                    <li class="email"><i class="fa fa-envelope"></i><br><a href="mailto: pedrohenrique635@hotmail.com">pedrohenrique635@hotmail.com</a></li>
+                    
+                    <li class="linkedin"><i class="fa fa-linkedin"></i><br><a href="https://br.linkedin.com/in/pedro-dib" target="_blank">linkedin.com/in/pedro-dib</a></li>
+                    <li class="github"><i class="fa fa-github"></i><br><a href="https://github.com/pedrodib" target="_blank">github.com/pedrodib</a></li>
+                   
+                </ul>
+            </div><!--//contact-container-->
+            <div class="education-container container-block">
+                <h2 class="container-block-title">Graduação</h2>
+                <div class="item">
+                    <h4 class="degree">Análise e Desenvolvimento de Sistemas</h4>
+                    <h5 class="meta">Universidade Nove de Julho</h5>
+                    <div class="time">2014 - 2016</div>
+                </div><!--//item-->
+                <div class="item">
+                    <h4 class="degree">Técnico em Informática</h4>
+                    <h5 class="meta">Senai</h5>
+                    <div class="time">2013 - 2014</div>
+                </div><!--//item-->
+            </div><!--//education-container-->
+            
+            <div class="languages-container container-block">
+                <h2 class="container-block-title">Idiomas</h2>
+                <ul class="list-unstyled interests-list">
+                    <li>Portugues <span class="lang-desc">(Nativo)</span></li>
+                    <li>Inglês <span class="lang-desc">(Intermediário)</span></li>
+                </ul>
+            </div><!--//interests-->
+            
+            <div class="interests-container container-block">
+                <h2 class="container-block-title">Interesses</h2>
+                <ul class="list-unstyled interests-list">
+                    <li>Games</li>
+                    <li>House Music</li>
+                </ul>
+            </div><!--//interests-->
+            
+        </div><!--//sidebar-wrapper-->
+        
+        <div class="main-wrapper">
+            
+            <section class="section summary-section">
+                <h2 class="section-title"><i class="fa fa-user"></i>EU</h2>
+                <div class="summary">
+                    <p>Olá! Se você chegou até aqui é porque você está em busca de um profissional para seu projeto. Bom, acredito que você pode parar por aqui.</p>
+                    <p>Se você precisa de um <strong>programador Full Stack PHP</strong>, está no lugar certo. Veja abaixo minha experiência, projetos, estudos e conhecimentos. </p>
+                </div><!--//summary-->
+            </section><!--//section-->
+            
+            <section class="section experiences-section">
+                <h2 class="section-title"><i class="fa fa-briefcase"></i>Experiências</h2>
+                
+                <div class="item">
+                    <div class="meta">
+                        <div class="upper-row">
+                            <h3 class="job-title">Analista de Sistemas</h3>
+                            <div class="time">2017 - Presente</div>
+                        </div><!--//upper-row-->
+                        <div class="company">Locaweb, São paulo</div>
+                    </div><!--//meta-->
+                    <div class="details">
+                        <p>Desenvolvimento Web Back-End, utilizando PHP e MySQL com grande volume de dados. Garantir a integridade e disponibilidade do software com análise e correção de erros. Criar novos módulos para o produto.</p>
+                    </div><!--//details-->
+                </div><!--//item-->
+                
+                <div class="item">
+                    <div class="meta">
+                        <div class="upper-row">
+                            <h3 class="job-title">Desenvolvedor Web</h3>
+                            <div class="time">2014 - 2017</div>
+                        </div><!--//upper-row-->
+                        <div class="company">Publitz, São Paulo</div>
+                    </div><!--//meta-->
+                    <div class="details">
+                        <p>Desenvolver aplicações web em ASP e PHP. </p>  
+                        
+                    </div><!--//details-->
+                </div><!--//item-->
+                
+                
+            </section><!--//section-->
+            
+            <section class="section projects-section">
+                <h2 class="section-title"><i class="fa fa-archive"></i>Projetos</h2>
+                <div class="intro">
+                    <p>Aqui estão os meus principais projetos.</p>
+                </div><!--//intro-->
+                <div class="item">
+                    <span class="project-title"><a href="https://acesse.buildings.com.br">Acesse Buildings</a></span> - <span class="project-tagline">Site e Sistema desenvolvido para empresa Buildings que realiza a busca de edifícios comerciais para interessados.</span>
+                    
+                </div><!--//item-->
+                <div class="item">
+                    <span class="project-title"><a href="https://webescritorios.com.br" target="_blank">Web Escritórios</a></span> - 
+                    <span class="project-tagline">Portal para busca de imóveis corporativos para venda ou locação.</span>
+                </div><!--//item-->
+                <div class="item">
+                    <span class="project-title"><a href="https://webindustrial.com.br" target="_blank">Web Industrial</a></span> - <span class="project-tagline">Portal para busca de imóveis industriais para venda ou locação.</span>
+                </div><!--//item-->
+                <div class="item">
+                    <span class="project-title"><a href="http://www.webgestor.com.br" target="_blank">WebGestor</a></span> - <span class="project-tagline">Sistema de Gestão para Imobiliárias. Abrangendo a parte de Compra e Venda, Visitas, Clientes, Integração com Portais, Sistema de Locação e Financeiro.</span>
+                </div><!--//item-->
+                <div class="item">
+                    <span class="project-title"><a href="https://allin.com.br" target="_blank">Allin</a></span> - <span class="project-tagline">Ferramenta de Marketing com abrangencia em Campanhas Tradicionais, Emails Transacionais, Posts em Rede Social e etc.</span>
+                </div><!--//item-->
+                <div class="item">
+                    <span class="project-title"><a href="http://cpro41498.publiccloud.com.br/api-campeonato-brasileiro/" target="_blank">Api do Campeonato Brasileiro</a></span> - <span class="project-tagline">Web Crawler que retorna a tabela do campeonato brasileiro em tempo real.</span>
+                </div><!--//item-->
+            </section><!--//section-->
+            
+            <section class="skills-section section">
+                <h2 class="section-title"><i class="fa fa-rocket"></i>Skills &amp; Proficiency</h2>
+                <div class="skillset">        
+                    <div class="item">
+                        <h3 class="level-title">PHP</h3>
+                        <div class="level-bar">
+                            <div class="level-bar-inner" data-level="100%">
+                            </div>                                      
+                        </div><!--//level-bar-->                                 
+                    </div><!--//item-->
+                    
+                    <div class="item">
+                        <h3 class="level-title">Javascript &amp; jQuery</h3>
+                        <div class="level-bar">
+                            <div class="level-bar-inner" data-level="100%">
+                            </div>                                      
+                        </div><!--//level-bar-->                                 
+                    </div><!--//item-->
+                    
+                    <div class="item">
+                        <h3 class="level-title">HTML5 &amp; CSS</h3>
+                        <div class="level-bar">
+                            <div class="level-bar-inner" data-level="95%">
+                            </div>                                      
+                        </div><!--//level-bar-->                                 
+                    </div><!--//item-->
+
+                    <div class="item">
+                        <h3 class="level-title">CakePHP</h3>
+                        <div class="level-bar">
+                            <div class="level-bar-inner" data-level="90%">
+                            </div>                                      
+                        </div><!--//level-bar-->                                 
+                    </div><!--//item-->
+                    
+                    <div class="item">
+                        <h3 class="level-title">VueJS</h3>
+                        <div class="level-bar">
+                            <div class="level-bar-inner" data-level="33%">
+                            </div>                                      
+                        </div><!--//level-bar-->                                 
+                    </div><!--//item-->
+                    <div class="item">
+                        <h3 class="level-title">Machine Learning</h3>
+                        <div class="level-bar">
+                            <div class="level-bar-inner" data-level="33%">
+                            </div>                                      
+                        </div><!--//level-bar-->                                 
+                    </div><!--//item-->
+                    
+                    
+                </div>  
+            </section><!--//skills-section-->
+            
+        </div><!--//main-body-->
+    </div>
+ 
+    <footer class="footer">
+        <div class="text-center">
+        </div><!--//container-->
+    </footer><!--//footer-->
+ 
+    <!-- Javascript -->          
+    <script type="text/javascript" src="assets/plugins/jquery-1.11.3.min.js"></script>
+    <script type="text/javascript" src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>    
+    <!-- custom js -->
+    <script type="text/javascript" src="assets/js/main.js"></script>            
+</body>
+</html> 
+
